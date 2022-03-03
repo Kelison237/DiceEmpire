fe1f271cb96845d63e95fde47aa3a148780e46eb
Kelison237 <77363856+Kelison237@users.noreply.github.com>
2022-03-03 03:15:49 +0100

    Add files via upload

--- Dice Empire/js/styling.js
+++ Dice Empire/js/styling.js
@@ -1,15 +1,20 @@
 
 let addBtn = document.querySelector('.addBtn');
 let popUp = document.querySelector('.popup');
+let darkBg = document.querySelector('.pageDark');
 addBtn.addEventListener('click',e => {
   console.log("Popup Clicked")
   if(popUp.style.display === "none") {
     popUp.style.display = "block";
+      // darkBg.style.display = "rgba(0,0,0,0)";
+    darkBg.style.display = "none";
   }else {
     popUp.style.display = "none";
+
   }
 })
 let close = document.querySelector('.closePopUp');
 close.addEventListener('click',e => {
   popUp.style.display = "none";
+      darkBg.style.display = "none";
 })
